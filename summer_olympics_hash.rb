7a83091403226deb428106abbe285ded8cc0f77e
Drue Johnson <drueby@gmail.com>
2018-07-26 13:53:49 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -9,7 +9,8 @@ puts create_olympics_hash
 
 def add_a_key_value_pair
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
-  
+  create_olympics_hash = {"Sydney" => 2000, "Athens" => 2004, "Beijing" => 2008, "London" => 2012
+  }
 end
 
 def iterate_through_hash
