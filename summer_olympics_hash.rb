3d08eaf6c59b900382f1a84e8169154971717591
Drue Johnson <drueby@gmail.com>
2018-07-26 13:59:17 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -11,9 +11,9 @@ def add_a_key_value_pair
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
   create_olympics_hash = {"Sydney" => 2000, "Athens" => 2004, "Beijing" => 2008, "London" => 2012
   }
-  olympic_place.push = "Atlanta"
-  olympic_year.push = 1996
-  create_olympics_hash [add_a_key_value_pair] = create_olympics_hash
+  olympic_place = "Atlanta"
+  olympic_year = 1996
+  create_olympics_hash [olympic_place] = olympic_year
 end
 add_a_key_value_pair
 def iterate_through_hash
